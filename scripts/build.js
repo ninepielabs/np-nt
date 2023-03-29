508a675006b84fceb66a5b74434ebfe22362f99d
xucong <xuerzong@gmail.com>
2023-03-29 11:58:39 +0800

    fix: build and exports

--- scripts/build.js
+++ scripts/build.js
@@ -1,12 +1,13 @@
 const { build } = require('esbuild')
 const path = require('path')
 const glob = require('fast-glob')
+const tsconfig = require('../tsconfig.json')
 
 const RUNTIME_ROOT_DIR = process.cwd()
 
 const entryPoints = [path.resolve('src/index.ts'), ...glob.sync(path.resolve('src/**/*.ts'))]
 
-const outdir = path.resolve(RUNTIME_ROOT_DIR, 'dist')
+const outdir = path.resolve(RUNTIME_ROOT_DIR, tsconfig.compilerOptions.outDir)
 
 build({
   entryPoints,
